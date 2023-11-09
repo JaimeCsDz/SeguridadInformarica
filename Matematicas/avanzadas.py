a9a59da24c4ebbbfd91bf245d493457dd013cffc
JaimeCsDz <107228118+JaimeCsDz@users.noreply.github.com>
2023-11-09 11:58:13 -0500

    Add files via upload

---
+++ Matematicas/avanzadas.py
@@ -0,0 +1,6 @@
+def pote(a,b):
+    return a**b
+
+
+def raiz_r(a):
+    return (a**(0.5))
---
+++ Matematicas/basicas.py
@@ -0,0 +1,11 @@
+def resultado_s(a, b):
+    return a + b
+
+def resta_r(a,b):
+    return a-b
+
+def multi_r(a,b):
+    return a*b
+
+def divi_r(a,b):
+    return a/b
